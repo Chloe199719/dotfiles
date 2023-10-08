e12c4081fa087087eee02297549019336a808c70
Chloe Pratas <chloevision97@gmail.com>
2023-10-08 23:52:56 +0200

    2nd try

---
+++ .config/nvim/lua/chloe/packer.lua
@@ -0,0 +1,82 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd([[packadd packer.nvim]])
+
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	--    	use({
+	--		"rose-pine/neovim",
+	--		as = "rose-pine",
+	--		config = function()
+	--			vim.cmd("colorscheme rose-pine")
+	--		end,
+	--	})
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use("nvim-treesitter/playground")
+	use("theprimeagen/harpoon")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		requires = {
+			-- LSP Support
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+
+			{ "neovim/nvim-lspconfig" },
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
+	use("github/copilot.vim")
+	use("neovim/nvim-lspconfig")
+
+	--use("jose-elias-alvarez/null-ls.nvim")
+	use("MunifTanjim/prettier.nvim")
+	use("christoomey/vim-tmux-navigator")
+	use({ "catppuccin/nvim", as = "catppuccin" })
+	use({
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				format_on_save = {
+					-- These options will be passed to conform.format()
+					timeout_ms = 500,
+					lsp_fallback = true,
+				},
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- Conform will run multiple formatters sequentially
+					python = { "isort", "black" },
+					-- Use a sub-list to run only the first available formatter
+					javascript = { { "prettierd", "prettier" } },
+					typescript = { { "prettierd", "prettier" } },
+					html = { { "prettierd", "prettier" } },
+					css = { { "prettierd", "prettier" } },
+					rust = { { "rustfmt" } },
+				},
+			})
+		end,
+	})
+	use("simrat39/rust-tools.nvim")
+end)
